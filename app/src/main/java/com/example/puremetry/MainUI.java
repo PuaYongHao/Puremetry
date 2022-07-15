436925de7bd628997d4cc72f7e83084d17926847
PuaYongHao <puayonghao@outlook.com>
2022-07-15 22:54:38 +0800

    Add onClick listener to MainUI

--- app/src/main/java/com/example/puremetry/MainUI.java
+++ app/src/main/java/com/example/puremetry/MainUI.java
@@ -4,21 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainUI extends AppCompatActivity {
+public class MainUI extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        Button resultsButton = (Button) findViewById(R.id.resultsButton);
+
+        startButton.setOnClickListener(this);
+        resultsButton.setOnClickListener(this);
     }
 
-    public void selectProfiles(View v){
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.startButton:
+                selectProfiles();
+                break;
+            case R.id.resultsButton:
+                selectResults();
+                break;
+        }
+    }
+
+    public void selectProfiles(){
         MainController.loadProfilesList(this);
     }
 
-    public void selectResults(View v){
+    public void selectResults(){
         MainController.loadReportsList(this);
     }
 
