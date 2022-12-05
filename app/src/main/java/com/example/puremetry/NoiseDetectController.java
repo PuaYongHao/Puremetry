d6242528f5af662a2ea59ef8c314d9af5f6b30c7
PuaYongHao <puayonghao@outlook.com>
2022-12-06 03:27:59 +0800

    Clean up the code and fix crash.

--- app/src/main/java/com/example/puremetry/NoiseDetectController.java
+++ app/src/main/java/com/example/puremetry/NoiseDetectController.java
@@ -3,79 +3,43 @@ package com.example.puremetry;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.MediaRecorder;
-import android.widget.Toast;
 
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import java.io.IOException;
 
 public class NoiseDetectController extends Activity {
 
-    public static final int APP_PERMISSION_RECORD_AUDIO = 1;
-    private static MediaRecorder mediaRecorder;
-    private static Context context;
+    private static String[] noise_description = {
+            "This is more likely to damage your hearing over time.",
+            "Find a quiet place.",
+            "It is good to go."
+    };
 
-    public static void detectNoiseLevel(Context c) {
-
-        context = c;
-
-        if (checkPermission()) {
-            mediaRecorder = new MediaRecorder();
-            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            mediaRecorder.setOutputFile("/dev/null/");
-
-            try {
-                mediaRecorder.prepare();
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            mediaRecorder.start();
-        }
-        else
-            requestPermission();
-
-    }
-
-    public void stop() {
-        if (mediaRecorder != null) {
-            mediaRecorder.stop();
-            mediaRecorder.reset();
-            mediaRecorder.release();
-            mediaRecorder = null;
-        }
-    }
-
-    public static int getAmplitude() {
-        if (mediaRecorder != null)
-            return (mediaRecorder.getMaxAmplitude());
-        else
-            return 0;
-    }
-
-    private static void requestPermission() {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
-                    Manifest.permission.RECORD_AUDIO)) {
-                Toast.makeText(context, "Please grant permission for audio.",
-                        Toast.LENGTH_SHORT).show();
-            }
-            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.RECORD_AUDIO}, APP_PERMISSION_RECORD_AUDIO);
-        } else if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
-        }
-        ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, APP_PERMISSION_RECORD_AUDIO);
-    }
-
-    public static boolean checkPermission(){
+    public static boolean checkPermission(Context context) {
         int result = ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO);
         int result2 = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         return result == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void loadHearingTest(Context context) {
+        Intent i = new Intent(context, HearingTestUI.class);
+        context.startActivity(i);
+    }
+
+    public static Boolean validateNoiseLevel(int noiseLevel) {
+        if (noiseLevel <= 20)
+            return true;
+        return false;
+    }
+
+    public static String updateDescription(int noiseLevel) {
+        if (noiseLevel >= 80)
+            return noise_description[0];
+        else if (noiseLevel <= 20)
+            return noise_description[2];
+        else
+            return noise_description[1];
     }
 
 }
--- app/src/main/java/com/example/puremetry/NoiseDetectUI.java
+++ app/src/main/java/com/example/puremetry/NoiseDetectUI.java
@@ -21,16 +21,13 @@ import androidx.core.content.ContextCompat;
 
 public class NoiseDetectUI extends AppCompatActivity implements View.OnClickListener {
 
-    private int noiseLevel;
-    private final String[] noise_description = {
-            "This is more likely to damage your hearing over time.",
-            "Find a quiet place.",
-            "It is good to go."
-    };
+    private int noiseLevel = 0;
     private ProgressBar noiseLevelProgressBar;
     private TextView noiseDetectTextView;
     private TextView noiseLevelTextView;
+    private Button continueButton;
     private Handler handler = new Handler();
+    private AudioRecord recorder;
 
     public static double REFERENCE = 0.00002;
     public static final int APP_PERMISSION_RECORD_AUDIO = 1;
@@ -48,11 +45,10 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
         noiseLevelTextView = findViewById(R.id.noiseLevelTextView);
 
         // Set OnClickListener for button
-        Button continueButton = findViewById(R.id.continueButton);
-
+        continueButton = findViewById(R.id.continueButton);
         continueButton.setOnClickListener(this);
+        continueButton.setEnabled(false);
 
-        noiseLevel = 0;
         noiseLevelProgressBar.setProgress(noiseLevel);
 
         detectNoiseLevel();
@@ -61,8 +57,9 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
     final Runnable updater = new Runnable() {
         @Override
         public void run() {
-            handler.postDelayed(this, 300);
-            detectNoiseLevel();
+            handler.postDelayed(this, 500);
+            if (recorder != null)
+                detectNoiseLevel();
         }
     };
 
@@ -70,40 +67,23 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.continueButton:
+                NoiseDetectController.loadHearingTest(this);
                 break;
         }
     }
 
-    public void displayNoiseLevel(int db) {
-        noiseLevel = db;
+    public void displayNoiseLevel() {
         noiseLevelProgressBar.setProgress(noiseLevel);
         noiseLevelTextView.setText(noiseLevel + " dB");
-        if (noiseLevel >= 80)
-            noiseDetectTextView.setText(noise_description[0]);
-        else if (noiseLevel < 20)
-            noiseDetectTextView.setText(noise_description[2]);
-        else
-            noiseDetectTextView.setText(noise_description[1]);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        handler.post(updater);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacks(updater);
+        String noise_description = NoiseDetectController.updateDescription(noiseLevel);
+        noiseDetectTextView.setText(noise_description);
     }
 
     public void detectNoiseLevel() {
-
-        if (checkPermission()) {
+        if (NoiseDetectController.checkPermission(this)) {
             int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
             bufferSize = bufferSize * 4;
-            AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+            recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
                     44100, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
 
             short data[] = new short[bufferSize];
@@ -121,30 +101,55 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
             }
             double x = average / bufferSize;
             recorder.release();
-            double db = 0;
+            double db;
             // calculating the pascal pressure based on the idea that the max amplitude (between 0 and 32767) is
             // relative to the pressure
-            double pressure = x / 51805.5336; //the value 51805.5336 can be derived from assuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
+            double pressure = x / 51805.5336; // the value 51805.5336 can be derived from assuming that x=32767=0.6325 Pa and x=1=0.00002 Pa (the reference value)
             db = (20 * Math.log10(pressure / REFERENCE));
             if (db > 0) {
-                displayNoiseLevel((int) db);
+                noiseLevel = (int) db;
+                displayNoiseLevel();
+                if (NoiseDetectController.validateNoiseLevel(noiseLevel))
+                    continueButton.setEnabled(true);
+                else
+                    continueButton.setEnabled(false);
             }
         } else
             requestPermission();
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        handler.post(updater);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(updater);
+    }
+
+    @Override
     public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
         switch (permsRequestCode) {
             case APP_PERMISSION_RECORD_AUDIO: {
-                if (grantResults.length > 0) {
+                if (grantResults.length > 1) {
                     boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
                     if (permissionToRecord && permissionToStore) {
                         Toast.makeText(getApplicationContext(), "Audio Permission is granted", Toast.LENGTH_SHORT).show();
                         detectNoiseLevel();
                     }
-                } else {
+                }
+                if ((grantResults.length == 1 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) ||
+                        ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                Manifest.permission.RECORD_AUDIO) ||
+                        ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                    detectNoiseLevel();
+                else {
                     Toast.makeText(getApplicationContext(), "Audio Permission is not granted", Toast.LENGTH_SHORT).show();
                 }
                 return;
@@ -165,12 +170,6 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
                                 ActivityCompat.requestPermissions(NoiseDetectUI.this, new String[]{Manifest.permission.RECORD_AUDIO}, APP_PERMISSION_RECORD_AUDIO);
                             }
                         })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
                         .create().show();
             } else
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, APP_PERMISSION_RECORD_AUDIO);
@@ -186,22 +185,10 @@ public class NoiseDetectUI extends AppCompatActivity implements View.OnClickList
                                 ActivityCompat.requestPermissions(NoiseDetectUI.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, APP_PERMISSION_RECORD_AUDIO);
                             }
                         })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
                         .create().show();
             } else
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, APP_PERMISSION_RECORD_AUDIO);
         }
-    }
-
-    public boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        int result2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        return result == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
     }
 
 }
